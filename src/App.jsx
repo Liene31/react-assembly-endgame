bc921ed7b99a74276c65ca04a1a3afe61e51ff03
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-01-31 18:20:59 +0100

    add header and game status

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+    </main>
+  );
 }
 
 export default App;
